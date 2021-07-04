ebdf12a7819762678fae0480d26c5a573a1100a9
Pete van Wesep <pvanwesep@sharespost.com>
2021-07-04 09:36:08 -0700

    Add env var for frequency

--- app/workers/trading_worker.rb
+++ app/workers/trading_worker.rb
@@ -3,8 +3,10 @@ require_relative "../../lib/client"
 class TradingWorker
   include Sidekiq::Worker
 
+  CHECK_FREQUENCY = Integer(ENV['CHECK_FREQUENCY'])
+
   def perform(*args)
     Trader.run
-    TradingWorker.perform_in(3.seconds)
+    TradingWorker.perform_in(CHECK_FREQUENCY.seconds)
   end
 end
