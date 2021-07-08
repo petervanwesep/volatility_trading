f732297d747aae5909af23df88c31a5d7cb9ac98
Pete van Wesep <pvanwesep@sharespost.com>
2021-07-08 16:46:59 -0700

    Update ALL

--- lib/client.rb
+++ lib/client.rb
@@ -58,7 +58,7 @@ module VolatilityTrading
   end
   module Client
     class Private
-      APPROXIMATE_ALL = 0.98
+      APPROXIMATE_ALL = 0.99
       MINIMUM_TOKEN_AMOUNT = 0.001
 
       def holding?(symbol:)
